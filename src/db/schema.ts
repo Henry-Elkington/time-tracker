a1b1790cdb12922e5f92cda426955430dd565422
Henry-Elkington <henry.d.elkington@gmail.com>
2023-03-07 14:05:33 -0700

    a

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,6 @@
 import { sqliteTable, text, integer, InferModel } from "drizzle-orm/sqlite-core";
-import { and, asc, desc, eq, or } from "drizzle-orm/expressions";
 import { drizzle } from "drizzle-orm/better-sqlite3";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
 
 export const users = sqliteTable("users", {
@@ -22,36 +22,7 @@ export const timeEntrys = sqliteTable("time_entrys", {
   userId: integer("user_id").references(() => users.id),
 });
 
-const sqlite = new Database("sqlite.db");
+const sqlite = new Database("./src/db/sqlite.db");
 export const db = drizzle(sqlite);
 
-// db.select().from(users).all();
-// db.select().from(users).where(eq(users.id, 42)).get();
-
-// // you can combine filters with and(...) or or(...)
-// db.select()
-//   .from(users)
-//   .where(and(eq(users.id, 42), eq(users.name, "Dan")))
-//   .all();
-
-// db.select()
-//   .from(users)
-//   .where(or(eq(users.id, 42), eq(users.id, 1)))
-//   .all();
-
-// // partial select
-// const result = db
-//   .select({
-//     field1: users.id,
-//     field2: users.name,
-//   })
-//   .from(users)
-//   .all();
-// const { field1, field2 } = result[0];
-
-// // limit offset & order by
-// db.select().from(users).limit(10).offset(10).all();
-// db.select().from(users).orderBy(users.name).all();
-// db.select().from(users).orderBy(desc(users.name)).all();
-// // you can pass multiple order args
-// db.select().from(users).orderBy(asc(users.name), desc(users.name)).all();
+migrate(db, { migrationsFolder: "./src/db/migrations" });
